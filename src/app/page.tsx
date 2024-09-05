1c87bfbe840e7602fdbb0a0884095edf55fa4b2b
YooJinRa <rarastudy@naver.com>
2024-09-05 23:13:23 +0900

    (#11) build안되는 현상으로 인해 page 파일 루트에 추가

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "@widgets/header/Header";
+import Sidebar from "@widgets/sidebar/Sidebar";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+    </>
+  );
+};
+
+export default page;
