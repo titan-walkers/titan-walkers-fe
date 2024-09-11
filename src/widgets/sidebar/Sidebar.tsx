76819d0eb1163ae8347d7236f43fbedbf3551daf
DESKTOP-RHNFT6Q\kimkyoungmoon <ruadans987@naver.com>
2024-09-12 00:48:52 +0900

    (#25) 미들웨어 수정, 메일쓰기 라우팅 추가

--- src/widgets/sidebar/Sidebar.tsx
+++ src/widgets/sidebar/Sidebar.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import React from "react";
 
-import * as S from "./Sidebar.style";
+import { useRouter } from "next/navigation";
 
+import * as S from "./Sidebar.style";
 const Sidebar = () => {
+
+  const router = useRouter();
+
+  const onClickCreateMail = () => {
+    router.push("/create-mail");
+  };
+
   return (
     <S.SidebarWrapper>
       <S.SidebarMenuSection>
-        <S.SidebarButton>메일쓰기</S.SidebarButton>
+        <S.SidebarButton onClick={onClickCreateMail}>메일쓰기</S.SidebarButton>
       </S.SidebarMenuSection>
     </S.SidebarWrapper>
   );
